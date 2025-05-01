789765e07266c0005e61fda14aca0a597620ffce
jevshyl <jevgenia-shylovich@edu.tbz.ch>
2025-05-01 16:53:16 +0200

    fix bugs

--- HTMLproject.js
+++ HTMLproject.js
@@ -1,5 +1,3 @@
-
-//no realoading when submitting smth
 document.getElementById("news-form").addEventListener("submit", function(event) {
     event.preventDefault(); 
 
@@ -7,12 +5,10 @@ document.getElementById("news-form").addEventListener("submit", function(event) 
     alert("Your Email was submitted!");
   });
 
-//menu
+
   function toggleMenu() {
     document.querySelector(".button-container").classList.toggle("active");
 }
-
-//clear mail
 
 document.addEventListener("DOMContentLoaded", function () {
   document
